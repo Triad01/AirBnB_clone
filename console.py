939a2a902044ba423159b6581085912e02eceb6b
Genius-Excel <geniusexcel9@gmail.com>
2024-01-10 12:39:11 +0100

    HBNB console class definition.

---
+++ console.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+""" This module contains the implementation of the interactive shell
+    for the HBNB console.
+"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+        This is the class definition for the HBNB interactive shell.
+    """
+
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """This class method handles the implementation of qutting the shell
+            line (str): this represents the command entered on the shell
+        """
+
+        return True
+
+    def emptyline(self):
+        """ This class method handles the implementation if user does
+            not enter a command.
+        """
+
+        pass
+
+    def do_EOF(self, line):
+        """ This class method handles the implementation of End-Of-File
+            condition.
+        Attributes:
+            line (str): this represents the command entered on the shell
+        """
+
+        return True
+    
+    # this metod is to be updated and documented later.
+    def do_help(self, arg: str) -> bool | None:
+        return super().do_help(arg)
