8871e5fc4574911636d3686177cdbcf4eef62a37
Triad01 <luckypee01@gmail.com>
2024-01-13 17:07:45 +0100

    fixed bug in user class

--- models/user.py
+++ models/user.py
@@ -5,9 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Represent the user model"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
