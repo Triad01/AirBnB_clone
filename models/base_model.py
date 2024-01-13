9a600ba7ddb321c2f6624acaf5eb3d889014e4a9
Triad01 <luckypee01@gmail.com>
2024-01-13 16:55:37 +0100

    fixed a bug in base model

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel():
 
     def __str__(self):
         """Provides string representation of the instance object"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}]"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updates modification time for 'update_at' method"""
